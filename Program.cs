95ec0ca567c4e197e5da167e0a08a5a1d9574d9a
Alexey_I <gafar@xaker.ru>
2023-02-11 20:23:38 +0300

    Final script

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿void print_array(int[] array)
+{
+    foreach(int element in array)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
+
+int[] create_array(int length, int lowest, int hightest)
+{
+    Random random = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(lowest, hightest + 1);
+    return array;
+}
+
+int[] copy_array(int[] array) 
+{
+    int array_length = array.Length;
+    int[] copy_array = new int[array_length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copy_array[i] = array[i];
+    }
+    return copy_array;
+
+}
+int[] array = create_array(10, 1, 99);
+
+Console.Write("Исходный массив: ");
+print_array(array);
+Console.Write("Копия массива: " );
+print_array(copy_array(array));
